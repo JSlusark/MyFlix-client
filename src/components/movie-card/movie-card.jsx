d90fe62001fa1ed8257fd32fead267ff5b0715ae
Jessica Slusark <slusark.jessica@gmail.com>
2024-01-11 18:37:03 +0100

    fixed backend update request when adding/removing movie to favorites

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -33,11 +33,45 @@ export const MovieCard = ({
 				(favMovie) => favMovie !== movieData
 			);
 			setFavoriteMovies(updatedFavorites);
+			fetch(
+				`https://shrouded-ocean-05047.herokuapp.com/users/${user.username}/movies/${movieData.id}`,
+				{ method: "DELETE", headers: { Authorization: `Bearer ${token}` } }
+			)
+				.then((response) => response.json())
+				.then((data) => {
+					console.log(data);
+					if (data) {
+						localStorage.setItem("user", JSON.stringify(data));
+						//location.reload();
+					} else {
+						alert("Failed to remove to favorites");
+					}
+				})
+				.catch((error) => {
+					alert(error.message);
+				});
 			//I want to be chnag
 		} else {
 			console.log("not favorited");
 			console.log(movieData);
 			setFavoriteMovies([...favoriteMovies, movieData]);
+			fetch(
+				`https://shrouded-ocean-05047.herokuapp.com/users/${user.username}/movies/${movieData.id}`,
+				{ method: "POST", headers: { Authorization: `Bearer ${token}` } }
+			)
+				.then((response) => response.json())
+				.then((data) => {
+					console.log(data);
+					if (data) {
+						localStorage.setItem("user", JSON.stringify(data));
+						//location.reload();
+					} else {
+						alert("Failed to add to favorites");
+					}
+				})
+				.catch((error) => {
+					alert(error.message);
+				});
 		}
 	};
 
