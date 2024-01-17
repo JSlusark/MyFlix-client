a6aee8beb05933a83d52bfad85065f286efd9fc2
Jessica Slusark <slusark.jessica@gmail.com>
2024-01-17 13:15:27 +0100

    Added logo to NavBar component

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,38 +4,42 @@ import Navbar from "react-bootstrap/Navbar";
 import { Link } from "react-router-dom";
 import { NavBarUnAuthenticated } from "./NavBarUnAuthenticated";
 import { NavBarAuthenticated } from "./NavBarAuthenticated";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import logo from "../../img/logo.png";
+import "./nav-bar.scss";
 
 export const NavBar = ({ user, onLoggedOut }) => {
+	console.log(logo);
 	return (
 		<Navbar
 			bg="dark"
 			variant="dark"
 			expand="lg"
+			className="p-3 ps-5 pe-5 align-items-baseline d-flex"
 		>
-			<Container
-			// style={{
-			// 	marginLeft: "42px",
-			// 	marginRight: "42px",
-			// 	padding: "5px",
-			// }}
+			<Navbar.Brand
+				as={Link}
+				to="/"
+				className="align-items-baseline fs-1 navbarTitle"
 			>
-				<Navbar.Brand
-					as={Link}
-					to="/"
-				>
-					My Movie
-				</Navbar.Brand>
-				<Navbar.Toggle aria-controls="basic-navbar-nav" />
-				<Navbar.Collapse id="basic-navbar-nav">
-					<Nav className="me-auto">
-						{user ? (
-							<NavBarAuthenticated onLoggedOut={onLoggedOut} />
-						) : (
-							<NavBarUnAuthenticated />
-						)}
-					</Nav>
-				</Navbar.Collapse>
-			</Container>
+				<img
+					fluid
+					alt=""
+					src={logo}
+					className="logo me-3 inline-block align-middle"
+				/>
+				My Movie List
+			</Navbar.Brand>
+			<Navbar.Toggle aria-controls="basic-navbar-nav" />
+			<Navbar.Collapse id="basic-navbar-nav">
+				<Nav className="me-auto d-flex align-items-baseline ">
+					{user ? (
+						<NavBarAuthenticated onLoggedOut={onLoggedOut} />
+					) : (
+						<NavBarUnAuthenticated />
+					)}
+				</Nav>
+			</Navbar.Collapse>
 		</Navbar>
 	);
 };
